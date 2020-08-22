82b113600cd0f08796ef0de77be7e99fee6bc2bb
Gajanan Sangle <gajanan.sangle@yahoo.com>
2020-08-22 02:32:10 -0400

    Home screen and menu changes

--- src/app/components/gallary/gallary.component.ts
+++ src/app/components/gallary/gallary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit,Component, OnInit, Renderer2 } from '@angular/core';
 import {BikeService } from '../../services/bike.service';
 
 @Component({
@@ -6,21 +6,42 @@ import {BikeService } from '../../services/bike.service';
   templateUrl: './gallary.component.html',
   styleUrls: ['./gallary.component.css']
 })
-export class GallaryComponent implements OnInit {
-  public bikes;
-
-  constructor(public bikeService: BikeService) { }
-
+export class GallaryComponent implements OnInit, AfterViewInit {
+  cards = [
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+    {img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'},
+  ];
+  
+  slides: any = [[]];
+  
+  constructor(private renderer: Renderer2) { }
+  
+  
+  chunk(arr: any, chunkSize: number) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
+  
   ngOnInit() {
-   this.getBikes();
+    this.slides = this.chunk(this.cards, 3);
   }
-
-  getBikes(){
-    this.bikeService.getBikes().subscribe(
-      data => {this.bikes = data},
-      err =>  console.error(err),
-      () => console.log('Bike Uploaded')
-    )
+  
+  ngAfterViewInit() {
+    const buttons = Array.from(document.querySelectorAll('.btn-floating'));
+    buttons.forEach((el: any) => {
+      this.renderer.removeClass(el, 'btn-floating');
+      this.renderer.addClass(el, 'px-3');
+      this.renderer.addClass(el.firstElementChild, 'fa-3x');
+    });
   }
-
 }
